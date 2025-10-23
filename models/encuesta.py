c78ea0a006b73c946ca1ae0d1f5e0dacb6dcffff
Ekasestao <ekaitzcampo2002@gmail.com>
2025-10-23 21:58:03 +0200

    Adding my class (Sorry to bussy to create another branch)

--- models/encuesta.py
+++ models/encuesta.py
@@ -2,9 +2,15 @@ from odoo import fields, models
 
 class Encuesta(models.Model):
     _name = "aplicacion_incidencias.encuesta"
-    _description = 'Tabla de Ekaitz'
 
     # [ CAMPOS SIMPLES ]
-
+    contenido = fields.Text(String="Encuesta")
+    fecha_creacion = fields.Date(String="Fecha de creacion")
 
     # [ CAMPOS RELACIONALES ]
+    # [primary key]
+    #id_encuesta = fields.One2one(comodel_name='aplicacion_incidencias.encuesta', string="Encuesta", required=True)
+    # [foreign key]
+    #id_incidencia = fields.One2one(comodel_name='aplicacion_incidencias.incidencia', string="Encuesta", required=False)
+    # [foreign key]
+    #id_empleado = fields.
