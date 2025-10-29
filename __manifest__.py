01481c83865ccc0cea828334897f6ac49d6907e0
Ekasestao <ekaitzcampo2002@gmail.com>
2025-10-29 10:53:38 +0100

    Crating security

--- __manifest__.py
+++ __manifest__.py
@@ -2,14 +2,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Aplicacion de Incidencias (Alex, Ekaitz, Kevin, Victor)',
+    'name': 'aplicacion_incidencias',
     'version': '1.0',
     'description': """
 Aplicación de incidencias del RETO 1 - CRUD de grupo compuesto por Alex, Ekaitz, Kevin y Victor.
     """,
     'depends': ['base'],
     'data': [
-
+        "security/ir.model.access.csv",
+        "security/aplicacion_incidencias_security.xml"
     ],
     'license': 'LGPL-3',
 }
