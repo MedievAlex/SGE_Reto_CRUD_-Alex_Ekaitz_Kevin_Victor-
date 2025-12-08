31bc38e7a0266783e60790732063653b74a43157
Ekasestao <ekaitzcampo2002@gmail.com>
2025-12-08 15:11:55 +0100

    Encuesta

--- models/encuesta.py
+++ models/encuesta.py
@@ -5,17 +5,17 @@ class Encuesta(models.Model):
     _description = "Tabla de Ekaitz"
 
     # [ CAMPOS SIMPLES ]
-    puntuacion = fields.Integer(string="Puntuación")
+    puntuacion = fields.Selection([
+        ('0', '0'),
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5')
+    ], string="Puntuación", default='5')
     comentario = fields.Text(string="Comentario")
     fecha_respuesta = fields.Date(string="Fecha", default=fields.Datetime.now)
 
     # [ CAMPOS RELACIONALES ]
     # [foreign key]
     x_incidencia_id = fields.Many2one(comodel_name="aplicacion_incidencias.incidencia", string="Incidencia", required=True, ondelete="cascade")
-
-    # [foreign key]
-    #empleado_id = fields.Many2one(
-        #comodel_name="aplicacion_incidencias.empleado",
-        #string="Empleado",
-        #required=True
-    #)
