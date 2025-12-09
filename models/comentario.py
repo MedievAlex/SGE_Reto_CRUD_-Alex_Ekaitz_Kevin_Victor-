4cb066e0aa712984d617628ea5e3fcc1ad13d270
MedievAlex <alexirazola@gmail.com>
2025-12-09 14:22:53 +0100

    working on comentario now

--- models/comentario.py
+++ models/comentario.py
@@ -5,9 +5,12 @@ class Comentario(models.Model):
     _description = "Tabla de Alex"
 
     # [ CAMPOS SIMPLES ]
+    name = fields.Text(string="Nombre:")
     contenido = fields.Text(string="Comentario:")
     fecha_creacion = fields.Date(string="Fecha de creacion:", readonly=True, default=fields.Datetime.now)
 
     # [ CAMPOS RELACIONALES ]
     # [foreign key]
     id_incidencia = fields.Many2one(comodel_name="aplicacion_incidencias.incidencia", string="Incidencia", required=True, ondelete="cascade")
+    inc_name = fields.Text(related="id_incidencia.name")
+    inc_estado_actual = fields.Boolean(related="id_incidencia.estado_actual")
--- models/incidencia.py
+++ models/incidencia.py
@@ -5,7 +5,7 @@ class Incidencia(models.Model):
     _description = "Tabla de Alex"
 
     # [ CAMPOS SIMPLES ]
-    titulo = fields.Text(string="Titulo de la incidencia:")
+    name = fields.Text(string="Titulo de la incidencia:")
     descripcion = fields.Text(string="Descripcion de la incidencia:")
     fecha_creacion = fields.Date(string="Fecha de creacion:", default=fields.Datetime.now)
     estado_actual = fields.Boolean(string="Incompleta/Completa:", default=False)
@@ -14,7 +14,10 @@ class Incidencia(models.Model):
     # [foreign key]
     estadisticas_ids = fields.Many2many(comodel_name="aplicacion_incidencias.estadisticas", relation="estadisticas_incidencia",
                                         column1="incidencia_id", column2="estadisticas_id", string="Estadísticas Relacionadas")
-    # [foreign key]
-    # id_empleado = fields.Many2one(comodel_name='hr.hr_employee', string="Incidencia", required=True, ondelete="cascade")
-    # [foreign key]
-    # id_departamento = fields.Many2one(comodel_name='hr.hr_department', string="Incidencia", required=True, ondelete="cascade")
+    id_comentario = fields.One2many(comodel_name="aplicacion_incidencias.comentario",
+                                    inverse_name="id_incidencia",
+                                    Sgtring="Comentario:",
+                                    required=False,
+                                    ondelete="cascade")
+    com_contenido = fields.Text(related="id_comentario.contenido")
+    com_fecha_creacion = fields.Date(related="id_comentario.fecha_creacion")
