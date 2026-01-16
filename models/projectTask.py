1001b9dd0fbdd2e5e1576469c701da404a70da24
MedievAlex <alexirazola@gmail.com>
2026-01-16 10:06:36 +0100

    created the model to work on it

---
+++ models/projectTask.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    # [ INCIDENCIA ]
+    #incidencia_id =
+
+    # [ ENCUESTA ]
+    #encuesta_id =
