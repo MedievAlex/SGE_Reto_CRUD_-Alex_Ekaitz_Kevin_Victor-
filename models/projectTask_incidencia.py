8301dc3791bc4a794fdfd76011547bb2653110ee
MedievAlex <alexirazola@gmail.com>
2026-01-20 11:59:46 +0100

    Esta vez si

--- models/projectTask_incidencia.py
+++ models/projectTask_incidencia.py
@@ -2,3 +2,9 @@ from odoo import fields, models
 
 class ProjectTask_incidencia(models.Model):
     _inherit = "project.task"
+
+    incidencia_id = fields.One2many(comodel_name='aplicacion_incidencias.incidencia', inverse_name='proyecto',
+                                    string='Incidencias')
+
+    nombre = fields.Char(comodel_name="Nombre", related="incidencia_id.name")
+    descripcion = fields.Char(comodel_name="Descripcion", related="incidencia_id.descripcion")
